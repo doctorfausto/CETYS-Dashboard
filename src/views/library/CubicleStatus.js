20605d78086187d0f5ccecbdf8b41d4ad7d2351b
Alecfut07 <ortega.cisneros.alec@gmail.com>
2021-10-25 13:50:14 -0700

    Adjustment to list cubicles
    (Before installing: npm install react-select)

---
+++ src/views/library/CubicleStatus.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function StatusCubicle() 
+{
+    var statusColors = [
+        {
+            value: 1,
+            label: "Disponible"
+        },
+        {
+            value: 2,
+            label: "Reservado"
+        },
+        {
+            value: 3,
+            label: "Ocupado"
+        }
+    ]
+
+    return(
+
+    )
+}
+export default StatusCubicle
--- src/views/library/Cubicles.js
+++ src/views/library/Cubicles.js
@@ -17,42 +17,70 @@ import { rgbToHex } from '@coreui/utils'
 import { DocsLink } from 'src/components'
 import 'src/scss/status.scss'
 
+// const CubicleView = () => {
+//   return (
+//     <div class="card">
+//       <table className="table w-100">
+//         <tbody>
+//         <tr>
+//           <td>
+//             <ul>
+//               <li class="status available">Disponible</li>
+//               <li class="status reserved">Reservado</li>
+//               <li class="status occupied">Ocupado</li>
+//             </ul>
+//           </td>
+//         </tr>
+//         <tr>
+//           <td>
+//             <CIcon icon={cilUser}/> 2 - 4
+//           </td>
+//         </tr>
+//         <tr>
+//           <td>Tiempo límite: 0 - 2 horas</td>
+//         </tr>
+//         <tr>
+//           <td>Tiempo restante: 02:00:00</td>
+//         </tr>
+//         </tbody>
+//       </table>
+//     </div>
+//   )
+// }
+
+// const CubicleRoom = ({className, children}) => {
+//   const classes = classNames(className, 'theme-color w-75 rounded mb-3')
+//   return (
+//     <CCol xl="2" md="4" sm="6" xs="12" className="mb-4">
+//       <div className={classes} style={{paddingTop: '75%'}}></div>
+//       {children}
+//       <CubicleView/>
+//     </CCol>
+//   )
+// }
+
 const CubicleView = () => {
   return (
-    <table className="table w-100">
-      <tbody>
-      <tr>
-        <td>
-          <ul>
-            <li class="status available">Available</li>
-            <li class="status reserved">Reserved</li>
-            <li class="status occupied">Occupied</li>
-          </ul>
-        </td>
-      </tr>
-      <tr>
-        <Link to="/library/cubicles/request-cubicle">
-          <button class='btn btn-outline-primary btn-block' type='button'>Request</button>
-        </Link>
-      </tr>
-      <tr>
-        <td>
-          <CIcon icon={cilUser}/> 2 - 4
-        </td>
-      </tr>
-      <tr>
-        <td className="text-muted">Limit Time: 0 - 2 hours</td>
-      </tr>
-      <tr>
-        <td className="text-muted">Time Left: 02:00:00</td>
-      </tr>
-      </tbody>
-    </table>
+    <div class="card" style={{width: '75%'}}>
+      <ul class="list-group list-group-flush">
+        <li class="status available list-group-item">Disponible</li>
+      </ul>
+      <div class="card-body">
+        <CIcon icon={cilUser}/> 2 - 4
+      </div>
+      <div class="list-group-item">
+        Tiempo Límite: 0 - 2 horas
+      </div>
+      <div class="list-group-item">
+        Tiempo restante: 02:00:00
+      </div>
+    </div>
   )
 }
 
+// paddingTop: '10%', paddingBottom: '10%', paddingLeft: '0%', paddingRight: '0%' 
 const CubicleRoom = ({className, children}) => {
-  const classes = classNames(className, 'theme-color w-75 rounded mb-3')
+  const classes = classNames(className, 'card-img-top w-75 rounded mb-3')
   return (
     <CCol xl="2" md="4" sm="6" xs="12" className="mb-4">
       <div className={classes} style={{paddingTop: '75%'}}></div>
@@ -67,54 +95,53 @@ const Cubicles = () => {
     <>
       <CCard>
         <CCardHeader>
-          Cubicles
-          <DocsLink href="https://coreui.io/docs/utilities/colors/"/>
+          Cubículos
         </CCardHeader>
         <CCardBody>
           <CRow>
-            <CubicleRoom className="bg-primary">
-              <h6>Cubicle 1</h6>
-            </CubicleRoom>
-            <CubicleRoom className="bg-secondary">
-              <h6>Cubicle 2</h6>
+            <CubicleRoom className="bg-success">
+              <h6>Cubículo 1</h6>
             </CubicleRoom>
             <CubicleRoom className="bg-success">
-              <h6>Cubicle 3</h6>
+              <h6>Cubículo 2</h6>
             </CubicleRoom>
-            <CubicleRoom className="bg-danger">
-              <h6>Cubicle 4</h6>
+            <CubicleRoom className="bg-success">
+              <h6>Cubículo 3</h6>
             </CubicleRoom>
-            <CubicleRoom className="bg-warning">
-              <h6>Cubicle 5</h6>
+            <CubicleRoom className="bg-success">
+              <h6>Cubículo 4</h6>
             </CubicleRoom>
-            <CubicleRoom className="bg-info">
-              <h6>Cubicle 6</h6>
+            <CubicleRoom className="bg-success">
+              <h6>Cubículo 5</h6>
             </CubicleRoom>
-            <CubicleRoom className="bg-light">
-              <h6>Cubicle 7</h6>
+            <CubicleRoom className="bg-success">
+              <h6>Cubículo 6</h6>
             </CubicleRoom>
-            <CubicleRoom className="bg-dark">
-              <h6>Cubicle 8</h6>
+            <CubicleRoom className="bg-success">
+              <h6>Cubículo 7</h6>
             </CubicleRoom>
-            <CubicleRoom className="bg-dark">
-              <h6>Cubicle 9</h6>
+            <CubicleRoom className="bg-success">
+              <h6>Cubículo 8</h6>
             </CubicleRoom>
-            <CubicleRoom className="bg-dark">
-              <h6>Cubicle 10</h6>
+            <CubicleRoom className="bg-success">
+              <h6>Cubículo 9</h6>
             </CubicleRoom>
-            <CubicleRoom className="bg-dark">
-              <h6>Cubicle 11</h6>
+            <CubicleRoom className="bg-success">
+              <h6>Cubículo 10</h6>
             </CubicleRoom>
-            <CubicleRoom className="bg-dark">
-              <h6>Cubicle 12</h6>
+            <CubicleRoom className="bg-success">
+              <h6>Cubículo 11</h6>
             </CubicleRoom>
-            <CubicleRoom className="bg-dark">
-              <h6>Cubicle 13</h6>
+            <CubicleRoom className="bg-success">
+              <h6>Cubículo 12</h6>
             </CubicleRoom>
-            <CubicleRoom className="bg-dark">
-              <h6>Cubicle 14</h6>
+            <CubicleRoom className="bg-success">
+              <h6>Cubículo 13</h6>
             </CubicleRoom>
-            <CubicleRoom className="bg-dark">
+            <CubicleRoom className="bg-success">
+              <h6>Cubículo 14</h6>
+            </CubicleRoom>
+            <CubicleRoom className="bg-success">
               <h6>Sala de video</h6>
             </CubicleRoom>
           </CRow>
