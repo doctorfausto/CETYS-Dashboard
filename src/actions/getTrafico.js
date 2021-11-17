8905be71e1516994a77a50339b5d9892a0608dea
FrogEmperor <oscar_tj2000@hotmail.com>
2021-11-16 16:05:32 -0800

    added dummy api

---
+++ src/actions/getTrafico.js
@@ -0,0 +1,64 @@
+const getTrafico = () => {
+    // TODO: API call
+    let data = [
+        {
+            'Biblioteca': 'CETYS Tijuana',
+            'id_reserva': '41939',
+            'Fecha_inicio': '2021-10-01 7:30:00',
+            'Fecha_fin': '2021-10-01 13:30:00',
+            'sala': 'Cubiculo 3',
+            'Bibliotecario': 'reservacion-tijuana',
+            'Matricula': 'T030233',
+            'Nombre_usuario': 'ALTAMIRANO CEREZO OSCAR',
+            'Carrera': 'INGENIERIA EN ENERGIAS RENOVABLES'
+        },
+        {
+            'Biblioteca': 'CETYS Tijuana',
+            'id_reserva': '41938',
+            'Fecha_inicio': '2021-10-01 10:30:00',
+            'Fecha_fin': '2021-10-01 12:30:00',
+            'sala': 'Cubiculo 8',
+            'Bibliotecario': 'reservacion-tijuana',
+            'Matricula': 'T030145',
+            'Nombre_usuario': 'GERARDO',
+            'Carrera': 'INGENIERIA EN CIENCIAS COMPUTACIONALES'
+        },
+        {
+            'Biblioteca': 'CETYS Tijuana',
+            'id_reserva': '41936',
+            'Fecha_inicio': '2021-10-01 7:30:00',
+            'Fecha_fin': '2021-10-01 13:30:00',
+            'sala': 'Cubiculo Z12',
+            'Bibliotecario': 'reservacion-tijuana',
+            'Matricula': 'T030233',
+            'Nombre_usuario': 'HERNANDEZ JUAN',
+            'Carrera': 'DOCENTE ASIGNATURA'
+        },
+        {
+            'Biblioteca': 'CETYS Tijuana',
+            'id_reserva': '41935',
+            'Fecha_inicio': '2021-10-01 7:30:00',
+            'Fecha_fin': '2021-10-01 13:30:00',
+            'sala': 'Cubiculo 1',
+            'Bibliotecario': 'reservacion-tijuana',
+            'Matricula': 'T029343',
+            'Nombre_usuario': 'ORTEGA ALEC',
+            'Carrera': 'INGENIERIA EN MECATRONICA'
+        },
+        {
+            'Biblioteca': 'CETYS Tijuana',
+            'id_reserva': '41934',
+            'Fecha_inicio': '2021-10-01 7:30:00',
+            'Fecha_fin': '2021-10-01 13:30:00',
+            'sala': 'Cubiculo 2',
+            'Bibliotecario': 'reservacion-tijuana',
+            'Matricula': 'T 39482',
+            'Nombre_usuario': 'VALENZUELA FAUTO MANUEL',
+            'Carrera': 'Trabajador'
+        }
+    ]
+
+    return data;
+}
+
+export default getTrafico;
---
+++ src/actions/getUser.js
@@ -0,0 +1,19 @@
+const getUser = () => {
+    // TODO: API call
+    let data = 
+        {
+            'Biblioteca': 'CETYS Tijuana',
+            'id_reserva': '41939',
+            'Fecha_inicio': '2021-10-01 7:30:00',
+            'Fecha_fin': '2021-10-01 13:30:00',
+            'sala': 'Cubiculo 3',
+            'Bibliotecario': 'reservacion-tijuana',
+            'Matricula': 'T030233',
+            'Nombre_usuario': 'ALTAMIRANO CEREZO OSCAR',
+            'Carrera': 'INGENIERIA EN ENERGIAS RENOVABLES'
+        };
+
+    return data;
+}
+
+export default getUser;
